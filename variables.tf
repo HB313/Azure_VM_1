f411fea9545ecb02f42e35356b55d8855831a5de
HB <hsh@HBs-MacBook-Air.local>
2025-06-03 16:26:25 +0200

    rajout d'un fichier .tfvars et modification du variables.tf

--- variables.tf
+++ variables.tf
@@ -1,57 +1,44 @@
-# variables.tf
-
 variable "subscription_id" {
   type = string
-  default = "a0246636-592d-44dc-b34b-d1d73dc69776"
 }
 
 variable "location" {
-  type    = string
-  default = "West Europe"
+  type = string
 }
 
 variable "resource_group_name" {
-  type    = string
-  default = "rg-demo"
+  type = string
 }
 
 variable "vnet_name" {
-  type    = string
-  default = "vnet-demo"
+  type = string
 }
 
 variable "vnet_address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
+  type = list(string)
 }
 
 variable "subnet_name" {
-  type    = string
-  default = "subnet-demo"
+  type = string
 }
 
 variable "subnet_prefix" {
-  type    = list(string)
-  default = ["10.0.1.0/24"]
+  type = list(string)
 }
 
 variable "nic_name" {
-  type    = string
-  default = "nic-demo"
+  type = string
 }
 
 variable "vm_name" {
-  type    = string
-  default = "vm-demo-verifitacionautomation"
+  type = string
 }
 
 variable "admin_username" {
-  type    = string
-  default = "azureuser"
+  type = string
 }
 
 variable "admin_password" {
-  type    = string
+  type      = string
   sensitive = true
-  default = "TFdemoPass123!"
 }
